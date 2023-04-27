cbc855206a0ba978192725b3e15defa07d21d700
Usha Kumari <usha@gmail.com>
2023-04-28 00:27:43 +0530

    nav

--- src/Components/TopHeader.js
+++ src/Components/TopHeader.js
@@ -6,20 +6,20 @@ const TopHeader = () => {
     <div className='row mx-0'>
         <div className='col-lg-3'>
             <div className='logo_wrapper'>
-                <img src={HeaderLogo}/>
+                <img alt="" src={HeaderLogo}/>
             </div>
         </div>
         <div className='col-lg-5'>
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <li class="nav-item" role="presentation">
-                <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button class="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Contact</button>
-            </li>
+            <ul className="nav nav-tabs header_tab" id="myTab" role="tablist">
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">Home</button>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Profile</button>
+                </li>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link" id="contact-tab" data-bs-toggle="tab" data-bs-target="#contact" type="button" role="tab" aria-controls="contact" aria-selected="false">Contact</button>
+                </li>
             </ul>
 
         </div>
